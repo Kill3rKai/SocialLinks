dd05a4316c5768ffbe6fd07487e624fdfa86eda7
Kill3rKai <89795034+Kill3rKai@users.noreply.github.com>
2026-01-06 18:57:13 +0000

    Delete Scripts directory

--- Scripts/progress.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".progress-fill").forEach(bar => {
-        const value = bar.dataset.progress;
-        requestAnimationFrame(() => {
-            bar.style.width = value + "%";
-        });
-    });
-});
