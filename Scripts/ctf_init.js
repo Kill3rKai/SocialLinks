d138e480d957cc831ce9944dcf61ed0cd2e63ba9
Kill3rKai <89795034+Kill3rKai@users.noreply.github.com>
2026-02-14 21:48:30 +0000

    Update ctf_init.js

--- Scripts/ctf_init.js
+++ Scripts/ctf_init.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let typed = '';
     document.addEventListener('keydown', (e) => {
         typed += e.key.toLowerCase();
-        if (typed.includes('ctf 1')) {
+        if (typed.includes('ctf1')) {
             window.location.href = 'Challanges\\ctf1.html';
         }
 
          if (typed.length > 20) typed = typed.slice(-20);
     });
+
 });
